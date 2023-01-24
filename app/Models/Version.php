9db6705a06eec4d14ae711ffb6f13143ffebf2ac
Marco Szulik <marco.szulik@goodbabyint.com>
2023-01-24 16:11:25 +0100

    add basic data structure

---
+++ app/Models/Version.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Version extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'version_nr',
+        'path',
+        'processed',
+        'media_id',
+    ];
+
+    /**
+     * Returns the media that the version belongs to.
+     */
+    public function Media(): BelongsTo
+    {
+        return $this->belongsTo(Media::class);
+    }
+}
---
+++ database/migrations/2023_01_24_141106_create_versions_table.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('versions', function (Blueprint $table) {
+            $table->id();
+            $table->integer('version_nr');
+            $table->string('path');
+            $table->boolean('processed')->default(0);
+            $table->foreignId('media_id')->constrained();
+            $table->timestamps();
+        });
+    }
+};
