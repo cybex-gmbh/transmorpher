d2325df2f1ed90a53b425cd73e191d560f942b36
Marco Szulik <marco.szulik@goodbabyint.com>
2023-02-21 17:08:09 +0100

    remove unnecessary comment

--- routes/api.php
+++ routes/api.php
@@ -19,7 +19,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->group(
     function () {
-        // General
         Route::get('/media/{identifier}/versions',[VersionController::class, 'getVersions']);
         Route::delete('/media/{identifier}', [VersionController::class, 'delete']);
         Route::patch('/media/{identifier}/version/{versionNumber}/set', [VersionController::class, 'setVersion']);
