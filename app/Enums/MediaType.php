cc1c0ad3403aa2aaaaca2025605e5a6cfd163704
Marco Szulik <marco.szulik@goodbabyint.com>
2023-05-08 11:01:05 +0200

    outsource some logic to the MediaType enum

--- app/Enums/MediaType.php
+++ app/Enums/MediaType.php
@@ -2,8 +2,69 @@
 
 namespace App\Enums;
 
+use App\Models\Media;
+use App\Models\UploadSlot;
+use App\Models\Version;
+use CdnHelper;
+use Throwable;
+use Transcode;
+
 enum MediaType: string
 {
     case IMAGE = 'image';
     case VIDEO = 'video';
+
+    public function getValidationRules(): string
+    {
+        return match ($this) {
+            MediaType::IMAGE => sprintf('mimes:%s', implode(',', ImageFormat::getFormats())),
+            MediaType::VIDEO => 'mimetypes:video/x-msvideo,video/mpeg,video/ogg,video/webm,video/mp4'
+        };
+    }
+
+    public function getUploadResponseMessage(): string
+    {
+        return match ($this) {
+            MediaType::IMAGE => 'Successfully added new image version.',
+            MediaType::VIDEO => 'Successfully uploaded video, transcoding job has been dispatched.'
+        };
+    }
+
+    public function handleSavedFile(string $basePath, UploadSlot $uploadSlot, string $filePath, Media $media, Version $version): array
+    {
+        return match ($this) {
+            MediaType::IMAGE => $this->handleSavedImage($basePath, $uploadSlot),
+            MediaType::VIDEO => $this->handleSavedVideo($filePath, $media, $version, $uploadSlot)
+        };
+    }
+
+    protected function handleSavedImage(string $basePath, UploadSlot $uploadSlot): array
+    {
+        if (CdnHelper::isConfigured()) {
+            try {
+                CdnHelper::invalidateImage($basePath);
+            } catch (Throwable) {
+                $success = false;
+                $response = 'Cache invalidation failed.';
+            }
+        }
+
+        // Only delete for image, since the UploadSlot will be needed inside the transcoding job.
+        $uploadSlot->delete();
+
+        return [
+            'success' => $success ?? true,
+            'response' => $response ?? $this->getUploadResponseMessage()
+        ];
+    }
+
+    protected function handleSavedVideo(string $filePath, Media $media, Version $version, UploadSlot $uploadSlot): array
+    {
+        $success = Transcode::createJob($filePath, $media, $version, $uploadSlot);
+
+        return [
+            'success' => $success,
+            'response' => $success ? $this->getUploadResponseMessage() : 'There was an error when trying to dispatch the transcoding job.'
+        ];
+    }
 }
--- app/Helpers/Upload.php
+++ app/Helpers/Upload.php
@@ -2,12 +2,10 @@
 
 namespace App\Helpers;
 
-use App\Enums\ImageFormat;
 use App\Enums\MediaStorage;
 use App\Enums\MediaType;
 use App\Http\Requests\UploadRequest;
 use App\Models\UploadSlot;
-use CdnHelper;
 use File;
 use FilePathHelper;
 use Illuminate\Http\JsonResponse;
@@ -16,8 +14,6 @@ use Pion\Laravel\ChunkUpload\Exceptions\UploadFailedException;
 use Pion\Laravel\ChunkUpload\Exceptions\UploadMissingFileException;
 use Pion\Laravel\ChunkUpload\Handler\HandlerFactory;
 use Pion\Laravel\ChunkUpload\Receiver\FileReceiver;
-use Throwable;
-use Transcode;
 
 class Upload
 {
@@ -50,17 +46,20 @@ class Upload
         ]);
     }
 
+    /**
+     * @param UploadedFile $uploadedFile
+     * @param UploadSlot   $uploadSlot
+     * @param MediaType    $type
+     *
+     * @return JsonResponse
+     */
     public static function saveFile(UploadedFile $uploadedFile, UploadSlot $uploadSlot, MediaType $type): JsonResponse
     {
-        $isImage = $type === MediaType::IMAGE;
         $user = $uploadSlot->User;
         $identifier = $uploadSlot->identifier;
 
         $media = $user->Media()->firstOrNew(['identifier' => $identifier, 'type' => $type]);
-        $mimeTypes = $isImage ?
-            sprintf('mimes:%s', implode(',', ImageFormat::getFormats()))
-            : 'mimetypes:video/x-msvideo,video/mpeg,video/ogg,video/webm,video/mp4';
-        $media->validateUploadFile($uploadedFile, $mimeTypes, $uploadSlot);
+        $media->validateUploadFile($uploadedFile, $type->getValidationRules(), $uploadSlot);
         $media->save();
 
         $versionNumber = $media->Versions()->max('number') + 1;
@@ -68,30 +67,10 @@ class Upload
         $fileName = FilePathHelper::createOriginalFileName($versionNumber, $uploadedFile->getClientOriginalName());
         $originalsDisk = MediaStorage::ORIGINALS->getDisk();
 
-        $success = true;
         if ($filePath = $originalsDisk->putFileAs($basePath, $uploadedFile, $fileName)) {
             $version = $media->Versions()->create(['number' => $versionNumber, 'filename' => $fileName]);
 
-            if ($isImage) {
-                // Invalidate cache and delete entry if failed.
-                if (CdnHelper::isConfigured()) {
-                    try {
-                        CdnHelper::invalidateImage($basePath);
-                    } catch (Throwable) {
-                        $success = false;
-                        $response = 'Cache invalidation failed.';
-                    }
-                }
-
-                // Only delete for image, since the UploadSlot will be needed inside the transcoding job.
-                $uploadSlot->delete();
-            } else {
-                $success = Transcode::createJob($filePath, $media, $version, $uploadSlot);
-
-                if (!$success) {
-                    $response = 'There was an error when trying to dispatch the transcoding job.';
-                }
-            }
+            ['success' => $success, 'response' => $response] = $type->handleSavedFile($basePath, $uploadSlot, $filePath, $media, $version);
         } else {
             $success = false;
             $response = 'Could not write media to disk.';
@@ -103,18 +82,19 @@ class Upload
             $version?->delete();
         }
 
-        // Delete chunk file and token.
+        // Delete chunk file.
         File::delete($uploadedFile);
 
         // Todo: to ensure that failed uploads don't pollute the image derivative cache, we would need a ready flag that is set to true when CDN is invalidated.
 
         return response()->json([
-            'success' => $success ?? true,
-            'response' => $response ?? $isImage ? 'Successfully added new image version.' : 'Successfully uploaded video, transcoding job has been dispatched.',
+            'success' => $success,
+            'response' => $response,
             'identifier' => $media->identifier,
             'version' => $versionNumber,
             'client' => $user->name,
-            'public_path' =>$isImage ? $basePath : null,
+            // Base path is only passed for images since the video is not available at this path yet.
+            'public_path' => $type === MediaType::IMAGE ? $basePath : null,
             'upload_token' => $uploadSlot->token
         ], 201);
     }
