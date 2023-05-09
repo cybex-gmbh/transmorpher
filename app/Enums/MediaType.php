7f52888520b3315d36a57e88766d1debf2984ea5
Marco Szulik <marco.szulik@goodbabyint.com>
2023-05-09 08:40:30 +0200

    refactor the version controller

--- app/Enums/MediaType.php
+++ app/Enums/MediaType.php
@@ -3,6 +3,7 @@
 namespace App\Enums;
 
 use App\Interfaces\MediaHandlerInterface;
+use Illuminate\Contracts\Filesystem\Filesystem;
 
 enum MediaType: string
 {
@@ -15,5 +16,13 @@ enum MediaType: string
     public function handler(): MediaHandlerInterface
     {
         return app(config(sprintf('transmorpher.media_handlers.%s', $this->value)));
+    }
+
+    public function getDerivativesDisk(): Filesystem
+    {
+        return match ($this) {
+            MediaType::IMAGE => MediaStorage::IMAGE_DERIVATIVES->getDisk(),
+            MediaType::VIDEO => MediaStorage::VIDEO_DERIVATIVES->getDisk(),
+        };
     }
 }
