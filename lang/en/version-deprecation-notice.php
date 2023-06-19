6286c4d11ebed5cb50a8552496e290db581ab3bf
Marco Szulik <marco.szulik@goodbabyint.com>
2023-06-19 14:53:58 +0200

    add a possibility to send a mail to all clients when a version is about to be deprecated.

---
+++ lang/en/version-deprecation-notice.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Version Deprecation Notice Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used in the Version Deprecation Notice
+    | email.
+    |
+    */
+    'subject' => 'Transmorpher: API v:apiVersion Deprecation Notice',
+    'title' => 'Version Deprecation Notice',
+    'version_soon_deprecated_message' => 'The support for the API version v:apiVersion will soon end.',
+    'update_client_implementations' => 'Please update your implementations to use a more recent API version.',
+    'thanks' => 'Thanks',
+    'your_team' => 'your :appName Team',
+];
