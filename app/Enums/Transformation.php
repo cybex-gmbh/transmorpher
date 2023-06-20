2dfac21c352c8dbb2a855051fe9b9ad0b4143087
Marco Szulik <marco.szulik@goodbabyint.com>
2023-06-20 12:06:10 +0200

    validate transformation parameters

--- app/Enums/Transformation.php
+++ app/Enums/Transformation.php
@@ -2,10 +2,53 @@
 
 namespace App\Enums;
 
+use InvalidArgumentException;
+
 enum Transformation: string
 {
     case WIDTH = 'w';
     case HEIGHT = 'h';
     case FORMAT = 'f';
     case QUALITY = 'q';
+
+    /**
+     * @param string|int $value
+     * @return string|int
+     */
+    public function validate(string|int $value): string|int
+    {
+        $valid = match ($this) {
+            self::WIDTH,
+            self::HEIGHT => filter_var($value, FILTER_VALIDATE_INT, ['options' => ['min_range' => 1]]),
+            self::FORMAT => in_array($value, ImageFormat::getFormats()) ? $value : false,
+            self::QUALITY => filter_var($value, FILTER_VALIDATE_INT, ['options' => ['min_range' => 1, 'max_range' => 100]])
+        };
+
+        if (!$valid) {
+            throw new InvalidArgumentException(sprintf('The provided value %s for the %s parameter is not valid.', $value, $this->name));
+        }
+
+        return $value;
+    }
+
+    /**
+     * @param string $transformations
+     * @return array|null
+     */
+    public static function arrayFromString(string $transformations): array|null
+    {
+        if (!$transformations) {
+            return null;
+        }
+
+        $transformationsArray = null;
+        $parameters = explode('+', $transformations);
+
+        foreach ($parameters as $parameter) {
+            [$key, $value] = explode('-', $parameter, 2);
+            $transformationsArray[$key] = Transformation::tryFrom($key)?->validate($value);
+        }
+
+        return $transformationsArray;
+    }
 }
