ed424762b9dad7d6a8a124165f5c011b79723710
Marco Szulik <marco.szulik@goodbabyint.com>
2023-07-03 10:37:55 +0200

    add a command to delete temporary ffmpeg folders

---
+++ app/Console/Commands/DeleteFfmpegTempFolders.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Console\Commands;
+
+use File;
+use Illuminate\Console\Command;
+
+class DeleteFfmpegTempFolders extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'ffmpeg:delete-temp';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Deletes all temporary Ffmpeg folders which are older than 1 day.';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle(): int
+    {
+        $directoriesDeleted = 0;
+
+        foreach (File::glob(base_path('ffmpeg-passes*')) as $directory) {
+            if (File::isDirectory($directory) && File::lastModified($directory) <= strtotime('-1 day', now()->timestamp)) {
+                File::deleteDirectory($directory) ? $directoriesDeleted++ : $this->error(sprintf('Could not delete directory "%s".', $directory));
+            }
+        }
+
+        $this->info(sprintf('Deleted %d directories.', $directoriesDeleted));
+
+        return Command::SUCCESS;
+    }
+}
