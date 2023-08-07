f8fa97d2445d93c8420652dbd301d75ebd41b572
mszulik <69617961+mszulik@users.noreply.github.com>
2023-08-07 17:43:03 +0200

    add versioning to the API (#16)

---
+++ routes/api/v1.php
@@ -0,0 +1,38 @@
+<?php
+
+use App\Helpers\SigningHelper;
+use App\Http\Controllers\V1\ImageController;
+use App\Http\Controllers\V1\UploadSlotController;
+use App\Http\Controllers\V1\VersionController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API v1 Routes
+|--------------------------------------------------------------------------
+|
+| Register API v1 routes here. The file has to be required by the default
+| api.php file.
+|
+*/
+
+Route::prefix('v1')->name('v1.')->group(function () {
+    Route::middleware('auth:sanctum')->group(
+        function () {
+            Route::get('/media/{media}/versions', [VersionController::class, 'getVersions']);
+            Route::delete('/media/{media}', [VersionController::class, 'delete']);
+            Route::patch('/media/{media}/version/{version}', [VersionController::class, 'setVersion']);
+
+            // Image
+            Route::get('/image/{media}/version/{version}/original', [ImageController::class, 'getOriginal']);
+            Route::get('/image/{media}/version/{version}/derivative/{transformations?}', [ImageController::class, 'getDerivativeForVersion']);
+            Route::post('/image/reserveUploadSlot', [UploadSlotController::class, 'reserveImageUploadSlot']);
+
+            // Video
+            Route::post('/video/reserveUploadSlot', [UploadSlotController::class, 'reserveVideoUploadSlot']);
+        }
+    );
+
+    Route::post('/upload/{uploadSlot}', [UploadSlotController::class, 'receiveFile']);
+    Route::get('publickey', fn(): string => SigningHelper::getPublicKey());
+});
