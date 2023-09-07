56de50971b6a0b3b308b34d45546ac64a14c4c1f
Marco Szulik <marco.szulik@goodbabyint.com>
2023-09-07 13:41:54 +0200

    touch folders and files since git doesn't care about dates

--- tests/Unit/DeleteFfmpegFoldersCommandTest.php
+++ tests/Unit/DeleteFfmpegFoldersCommandTest.php
@@ -13,15 +13,16 @@ class DeleteFfmpegFoldersCommandTest extends TestCase
      * @test
      * @dataProvider folderDataProvider
      */
-    public function ensureCorrectFoldersAreDeleted(string $directory, bool $expectation)
+    public function ensureCorrectFoldersAreDeleted(string $baseName, string $creationDate, bool $expectation)
     {
+        $path = base_path(sprintf('tests/data/ffmpeg-folders/%s', $baseName));
+
         $this->travelTo(Carbon::create(2000, 01, 01));
+        touch($path, Carbon::parse($creationDate)->timestamp);
 
         $this->assertEquals(
             $expectation,
-            app(DeleteFfmpegTempFolders::class)->directoryShouldBeDeleted(
-                base_path(sprintf('tests/data/ffmpeg-folders/%s', $directory))
-            )
+            app(DeleteFfmpegTempFolders::class)->directoryShouldBeDeleted($path)
         );
     }
 
@@ -30,18 +31,22 @@ class DeleteFfmpegFoldersCommandTest extends TestCase
         return [
             'directoryOlderThanADay' => [
                 'directory' => 'ffmpeg-passes_directoryOlderThanADay',
+                'creationDate' => '1999-12-31',
                 'expectation' => true
             ],
             'directoryYoungerThanADay' => [
-                'directory' => 'ffmpeg-passes_directoryyoungerThanADay',
+                'directory' => 'ffmpeg-passes_directoryYoungerThanADay',
+                'creationDate' => '2000-01-01',
                 'expectation' => false
             ],
             'fileOlderThanADay' => [
                 'directory' => 'ffmpeg-passes_fileOlderThanADay',
+                'creationDate' => '2000-01-01',
                 'expectation' => false
             ],
             'fileYoungerThanADay' => [
                 'directory' => 'ffmpeg-passes_fileYoungerThanADay',
+                'creationDate' => '2000-01-01',
                 'expectation' => false
             ],
         ];
