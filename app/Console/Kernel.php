cfcdfde85f4fd163161897e9227698a25c3c2180
Marco Szulik <marco.szulik@goodbabyint.com>
2024-03-20 10:22:57 +0100

    use ffmpeg's new 'temporary_directory' configuration option

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,7 +2,6 @@
 
 namespace App\Console;
 
-use App\Console\Commands\DeleteFfmpegTempFolders;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -16,7 +15,6 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        $schedule->command(DeleteFfmpegTempFolders::class)->daily();
     }
 
     /**
@@ -26,7 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function commands(): void
     {
-        $this->load(__DIR__.'/Commands');
+        $this->load(__DIR__ . '/Commands');
 
         require base_path('routes/console.php');
     }
--- app/Models/UploadSlot.php
+++ app/Models/UploadSlot.php
@@ -24,6 +24,7 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
  * @property \Illuminate\Support\Carbon|null $created_at
  * @property \Illuminate\Support\Carbon|null $updated_at
  * @property-read \App\Models\User $User
+ * @property-read mixed $is_valid
  * @method static Builder|UploadSlot newModelQuery()
  * @method static Builder|UploadSlot newQuery()
  * @method static Builder|UploadSlot query()
