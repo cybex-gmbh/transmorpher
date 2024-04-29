16f94b0c45a322b80c7100a332178954929e9625
Marco Szulik <marco.szulik@goodbabyint.com>
2024-04-29 15:04:11 +0200

    fix pullpreview seeder

--- database/seeders/PullpreviewSeeder.php
+++ database/seeders/PullpreviewSeeder.php
@@ -15,7 +15,7 @@ class PullpreviewSeeder extends Seeder
      */
     public function run(): void
     {
-        Artisan::call('create:user pullpreview pullpreview@example.com');
+        Artisan::call('create:user pullpreview pullpreview@example.com http://pullpreview.test/transmorpher/notifications');
 
         DB::table('personal_access_tokens')->where('id', 1)->update(['token' => env('PULLPREVIEW_TRANSMORPHER_AUTH_TOKEN_HASH')]);
     }
