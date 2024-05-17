6640ae58ddfc5baee6fa8edb71f9963cee82734a
Fabian Holy <fabian.holy@cybex-online.com>
2024-05-17 11:26:59 +0200

    refactor routing paths and update middleware.

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -8,19 +8,18 @@ use Illuminate\Support\Facades\Route;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php',
-        commands: __DIR__.'/../routes/console.php',
+        // Using base_path() instead of __DIR__ because it's more uniform.
+        web: base_path('routes/web.php'),
+        api: base_path('routes/api.php'),
+        commands: base_path('routes/console.php'),
         health: '/up',
         then: function () {
-            Route::middleware('delivery')
+            Route::middleware(SubstituteBindings::class)
                 ->group(base_path('routes/delivery.php'));
         },
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->appendToGroup('delivery', [
-            SubstituteBindings::class
-        ]);
+        //
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
