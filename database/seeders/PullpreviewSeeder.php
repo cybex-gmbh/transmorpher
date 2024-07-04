0c220f062fe8c41610e6c15388c4604317d1ccb1
Fabian Holy <fabian.holy@cybex-online.com>
2024-07-04 11:52:12 +0200

    update PullPreview seeder.

--- database/seeders/PullpreviewSeeder.php
+++ database/seeders/PullpreviewSeeder.php
@@ -19,7 +19,7 @@ class PullpreviewSeeder extends Seeder
             'name' => env('SEED_USER_NAME'),
             'email' => env('SEED_USER_EMAIL'),
             'api_url' => sprintf('http://%s/%s', env('CLIENT_CONTAINER_NAME'), env('CLIENT_NOTIFICATION_ROUTE', 'transmorpher/notifications')),
-            'password' => env('SEED_USER_PASSWORD')
+            '--password' => env('SEED_USER_PASSWORD')
         ]);
 
         DB::table('personal_access_tokens')->where('id', 1)->update(['token' => env('TRANSMORPHER_AUTH_TOKEN_HASH')]);
